3f7f9df1d921d5a9f5efd728f1af8d436dc44ca6
YNSHKI <joymanfreefire@gmail.com>
2025-09-10 21:56:07 +0200

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~IitFDaCD#iyxHr5q6JG9c7TEsnXjAzqWBsuYcj3npIKgqgBO1mtE",  // add your Session Id here
   
   OWNER_NUMBER: "263714757857", // put your phone number here
   
