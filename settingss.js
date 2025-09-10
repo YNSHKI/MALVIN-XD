b94966102e6547ca5701c2eca35449a8b1798e4c
YNSHKI <joymanfreefire@gmail.com>
2025-09-10 21:57:47 +0200

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -2,9 +2,9 @@
 module.exports = {
   SESSION_ID: "malvin~IitFDaCD#iyxHr5q6JG9c7TEsnXjAzqWBsuYcj3npIKgqgBO1mtE",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "27674034187", // put your phone number here
   
-  PREFIX: ".", // prefix (e.g., ., /, !, *)
+  PREFIX: "/", // prefix (e.g., ., /, !, *)
   
   TIMEZONE: "Africa/Harare" //put your country timeZone....leave blank if u don't know.
 };
